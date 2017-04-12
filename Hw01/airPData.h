f7a96fbbf9f39d266a521790c10c7f8cfca1977f
Nick Conklin <nconkl1@outlook.com>
2017-04-12 09:35:49 -0400

    Updated airPData

--- Hw01/airPData.h
+++ Hw01/airPData.h
@@ -1,11 +1,8 @@
 typedef struct airPData
 {
-	char *siteNumber; //FAA Site Number
 	char *LocID; //Airport’s ‘‘Short Name’’, ie MCO
 	char *fieldName; //Airport Name
 	char *city; //Associated City
-	char *state; //State
-	char *latitude; //Latitude
-	char *longitude; //Longitude
-	char controlTower;//Control Tower (Y/N)
+	float latitude; //Latitude
+	float longitude; //Longitude
 } airPData;
